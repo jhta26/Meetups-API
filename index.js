bb19291ae281719f90329f7ede58e136d6360fb2
jhta26 <hsujasonf@gmail.com>
2017-11-28 15:55:01 -0800

    setting up knex an index files

---
+++ index.js
@@ -0,0 +1,41 @@
+const { JWT_KEY } = require('./env');
+const express = require('express');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const cors = require('cors');
+const jwt = require('express-jwt');
+const app = express();
+const port = process.env.PORT || 8000;
+const authentication = require('./routes/authentication');
+const participants = require('./routes/participants');
+const meetups = require('./routes/meetups');
+const users = require('./routes/users');
+
+app.use(bodyParser.json());
+
+app.use(
+    jwt({
+        secret: JWT_KEY,
+        requestProperty: 'jwt.payload',
+        credentialsRequired: false,
+        audience: 'MeetupsMap',
+        issuer: 'MeetupsMap'
+    })
+);
+
+app.use((req, res, next) => {
+    let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
+    req.authenticatedUserId =
+        Number.isFinite(authUserId) && authUserId > 0 ? authUserId : null;
+    next();
+});
+
+app.use(meetups);
+app.use(participants);
+app.use(users);
+app.use(authenticationRouter);
+app.all('*', (req, res, next) => res.sendStatus(404));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
