36c41b583651cd3890c3fa1cc21d9b7b8f459c95
jhta26 <hsujasonf@gmail.com>
2017-11-28 16:20:27 -0800

    adding migrations

---
+++ migrations/20171128161522_users.js
@@ -0,0 +1,14 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('users', table => {
+        table.increments()
+        table.string('name').notNullable().defaultTo('')
+        table.string('username').notNullable.unique()
+        table.specificType('hashed_password', 'char(60)').notNullable();
+        table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
+        table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
+    })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('users');
+};
