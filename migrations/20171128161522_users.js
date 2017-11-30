dd52b12f185bc4cf4e18a82afe4479650a34c6dd
jhta26 <hsujasonf@gmail.com>
2017-11-30 11:48:55 -0800

    adding in bar info

--- migrations/20171128161522_users.js
+++ migrations/20171128161522_users.js
@@ -3,7 +3,7 @@ exports.up = function(knex) {
         table.increments()
         table.string('name').notNullable().defaultTo('')
         table.string('username').notNullable().unique()
-        table.number('bar_info').notNullable().defaultTo(0)
+        table.integer('bar_info').notNullable().defaultTo(0)
         table.specificType('hashed_password', 'char(60)').notNullable();
         table.timestamp('created_at').notNullable().defaultTo(knex.fn.now());
         table.timestamp('updated_at').notNullable().defaultTo(knex.fn.now());
