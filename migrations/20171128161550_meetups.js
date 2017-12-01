9bd6d10fa924d5834e513f34e92507245adfbae1
jhta26 <hsujasonf@gmail.com>
2017-11-30 16:06:34 -0800

    need to change migrations

--- migrations/20171128161550_meetups.js
+++ migrations/20171128161550_meetups.js
@@ -1,22 +1,21 @@
-
 exports.up = function(knex) {
-  return knex.schema.createTable('meetups',table=>{
-  	table.increments()
-  	table
+    return knex.schema.createTable('meetups', table => {
+        table.increments()
+        table
             .integer('user_id')
             .notNullable()
             .references('id')
             .inTable('users')
             .index()
             .onDelete('CASCADE')
-  	table.string('name').notNullable().defaultTo('')
-  	table.string('creator').notNullable().defaultTo('')
-  	table.string('location_meetup').notNullable().defaultTo('')
-  	table.string('time_meetup').notNullable().defaultTo('')
-  	table.string('status').notNullable().defaultTo('')
-  })
+        table.string('name').notNullable().defaultTo('')
+        table.string('creator').notNullable().defaultTo('')
+        table.string('location_meetup').notNullable().defaultTo('')
+        table.string('time_meetup').notNullable().defaultTo('')
+        table.string('status').notNullable().defaultTo('')
+    })
 };
 
 exports.down = function(knex) {
-  return knex.schema.dropTable('meetups')
+    return knex.schema.dropTable('meetups')
 };
