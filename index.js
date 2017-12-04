ffd0669b001fe4ac56e08a6f9f1d1a0c36798b0b
jhta26 <hsujasonf@gmail.com>
2017-12-04 14:02:43 -0800

    done with requirements for backend

--- index.js
+++ index.js
@@ -7,24 +7,23 @@ const jwt = require('express-jwt');
 const app = express();
 const port = process.env.PORT || 8000;
 
-
 app.use(bodyParser.json());
 
 app.use(
-    jwt({
-        secret: JWT_KEY,
-        requestProperty: 'jwt.payload',
-        credentialsRequired: false,
-        audience: 'MeetupsMap',
-        issuer: 'MeetupsMap'
-    })
+  jwt({
+    secret: JWT_KEY,
+    requestProperty: 'jwt.payload',
+    credentialsRequired: false,
+    audience: 'MeetupsMap',
+    issuer: 'MeetupsMap'
+  })
 );
 
 app.use((req, res, next) => {
-    let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
-    req.authenticatedUserId =
-        Number.isFinite(authUserId) && authUserId > 0 ? authUserId : null;
-    next();
+  let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
+  req.authenticatedUserId =
+    Number.isFinite(authUserId) && authUserId > 0 ? authUserId : null;
+  next();
 });
 
 const authentication = require('./routes/authentication');
@@ -38,7 +37,7 @@ app.use(authentication);
 app.all('*', (req, res, next) => res.sendStatus(404));
 
 app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+  console.log(`Listening on port ${port}`);
 });
 
-module.exports = app
+module.exports = app;
