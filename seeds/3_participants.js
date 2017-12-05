6284fab545b89ed4ae36575ee82fbb513de39f1c
jhta26 <hsujasonf@gmail.com>
2017-12-04 17:08:29 -0800

    chaning seeds

--- seeds/3_participants.js
+++ seeds/3_participants.js
@@ -1,49 +1,48 @@
 exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('participants').del().then(function() {
-    // Inserts seed entries
-    return knex('participants').insert([
-      {
-        user_id: 1,
-        meetup_id: 1,
-        status: 'ACCEPT',
-        current_lat: 37.7881439,
-        current_lon: -122.4017237,
-        time_remaining: '',
-        time_arrived: '',
-        already_there: ''
-      },
+    // Deletes ALL existing entries
+    return knex('participants').del().then(function() {
+        // Inserts seed entries
+        return knex('participants').insert([{
+                user_id: 1,
+                meetup_id: 1,
+                status: 'ACCEPT',
+                current_lat: 37.7881439,
+                current_lon: -122.4017237,
+                time_remaining: '',
+                time_arrived: '',
+                already_there: ''
+            },
 
-      {
-        user_id: 2,
-        meetup_id: 1,
-        status: 'ACCEPT',
-        current_lat: 37.7817098,
-        current_lon: -122.3961356,
-        time_remaining: '',
-        time_arrived: '',
-        already_there: ''
-      },
-      {
-        user_id: 1,
-        meetup_id: 3,
-        status: 'ACCEPT',
-        current_lat: 37.7881439,
-        current_lon: -122.4017237,
-        time_remaining: '',
-        time_arrived: '',
-        already_there: ''
-      },
-      {
-        user_id: 1,
-        meetup_id: 4,
-        status: 'ACCEPT',
-        current_lat: 37.7881439,
-        current_lon: -122.4017237,
-        time_remaining: '',
-        time_arrived: '',
-        already_there: ''
-      }
-    ]);
-  });
+            {
+                user_id: 2,
+                meetup_id: 1,
+                status: 'ACCEPT',
+                current_lat: 37.7817098,
+                current_lon: -122.3961356,
+                time_remaining: '',
+                time_arrived: '',
+                already_there: ''
+            },
+            {
+                user_id: 1,
+                meetup_id: 3,
+                status: 'ACCEPT',
+                current_lat: 37.7881439,
+                current_lon: -122.4017237,
+                time_remaining: '',
+                time_arrived: '',
+                already_there: ''
+            },
+            {
+                user_id: 1,
+                meetup_id: 4,
+                status: 'ACCEPT',
+                current_lat: 37.7881439,
+                current_lon: -122.4017237,
+                time_remaining: '',
+                time_arrived: '',
+                already_there: ''
+            }
+        ]);
+    });
 };
