e191aa8dc1d1815a62fa2579311cb66f8bac30ee
jhta26 <hsujasonf@gmail.com>
2017-12-14 19:02:55 -0800

    almost done with sockets

--- index.js
+++ index.js
@@ -4,27 +4,50 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const jwt = require('express-jwt');
+var socketio = require('socket.io');
 const app = express();
+var http = require('http')
+var server = http.Server(app);
 const port = process.env.PORT || 8000;
 
+var websocket = socketio(server);
+const db = require('./knex');
+server.listen(8000,()=>console.log('listening'))
+
+
+websocket.on('connection', (socket) => {
+    socket.on('location', (location) => onLocationReceived(location, socket));
+});
+
+async function onLocationReceived(location, socket) {
+   
+   
+        db('users').update(location, 'current_lat_lon');
+        var parts = await db.from('users').innerJoin('participants', 'users.id', 'participants.user_id')
+        socket.emit('participants', parts)
+
+}
 app.use(bodyParser.json());
 
 app.use(
-  jwt({
-    secret: JWT_KEY,
-    requestProperty: 'jwt.payload',
-    credentialsRequired: false,
-    audience: 'MeetupsMap',
-    issuer: 'MeetupsMap'
-  })
+    jwt({
+        secret: JWT_KEY,
+        requestProperty: 'jwt.payload',
+        credentialsRequired: false,
+        audience: 'MeetupsMap',
+        issuer: 'MeetupsMap'
+    })
 );
 
 app.use((req, res, next) => {
-  let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
-  req.authenticatedUserId =
-    Number.isFinite(authUserId) && authUserId > 0 ? authUserId : null;
-  next();
+    let authUserId = req.jwt ? req.jwt.payload.sub : undefined;
+    req.authenticatedUserId =
+        Number.isFinite(authUserId) && authUserId > 0 ? authUserId : null;
+    next();
 });
+
+
+
 
 const authentication = require('./routes/authentication');
 const participants = require('./routes/participants');
@@ -37,8 +60,10 @@ app.use(users);
 app.use(authentication);
 app.all('*', (req, res, next) => res.sendStatus(404));
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}`);
+
+
+server.listen(port, () => {
+    console.log(`Listening on port ${port}`);
 });
 
 module.exports = app;
